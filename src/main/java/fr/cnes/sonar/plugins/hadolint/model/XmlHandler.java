98ca90a9904be0234026d5831323c8bf50e57e7d
Alexis Chatillon <46956548+Sancretor@users.noreply.github.com>
2020-09-02 14:45:12 +0000

    Configure XStream security feature && define only needed converters

--- src/main/java/fr/cnes/sonar/plugins/hadolint/model/XmlHandler.java
+++ src/main/java/fr/cnes/sonar/plugins/hadolint/model/XmlHandler.java
@@ -18,7 +18,15 @@ package fr.cnes.sonar.plugins.hadolint.model;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.mapper.MapperWrapper;
+import com.thoughtworks.xstream.security.NoTypePermission;
+import com.thoughtworks.xstream.security.NullPermission;
+import com.thoughtworks.xstream.security.PrimitiveTypePermission;
+import com.thoughtworks.xstream.converters.basic.NullConverter;
+import com.thoughtworks.xstream.converters.basic.StringConverter;
+import com.thoughtworks.xstream.converters.collections.CollectionConverter;
+import com.thoughtworks.xstream.converters.reflection.ReflectionConverter;
 import java.io.InputStream;
+import java.util.Collection;
 
 /**
  * Class used to unmarshal ShellCheck xml file (results and rules definition).
@@ -53,8 +61,31 @@ public class XmlHandler {
                     }
                 };
             }
+            // only register the converters we need
+            // other converters generate a private access warning in the console on Java9+...
+            @Override
+            protected void setupConverters() {
+                registerConverter(new NullConverter(), PRIORITY_VERY_HIGH);
+                registerConverter(new StringConverter(), PRIORITY_NORMAL);
+                registerConverter(new CollectionConverter(getMapper()), PRIORITY_NORMAL);
+                registerConverter(new ReflectionConverter(getMapper(), getReflectionProvider()), PRIORITY_VERY_LOW);
+            }
         };
+
+        // Configure XStream security feature to avoid warnings
+        // clear out existing permissions and set own ones
+        xStream.addPermission(NoTypePermission.NONE);
+        // allow some basics
+        xStream.addPermission(NullPermission.NULL);
+        xStream.addPermission(PrimitiveTypePermission.PRIMITIVES);
+        xStream.allowTypeHierarchy(Collection.class);
+        // allow any type from the same package
+        xStream.allowTypesByWildcard(new String[] {
+            "fr.cnes.sonar.plugins.hadolint.**"
+        });
+
         xStream.processAnnotations(cls);
+
         return xStream.fromXML(file);
     }
 
